e5c1a121950bd4f788708f29ddcf9105f8a13c36
znjs <jasrikar@gmail.com>
2022-03-23 21:13:29 +0530

    fix: removed comments

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,8 +4,6 @@ import { ProductCard } from "../ProductCard/ProductCard";
 
 function Products() {
   const { productsState } = useFilter();
-  // console.log(productsState.products);
-  // console.log(data);
   return (
     <div className="fx fx-wrap">
       {productsState.products.length > 0 &&
